9d7e23c4612a2bcedd81a1468a60d245ba0ab7e2
vugginanitin <vugginanitin007@gmail.com>
2025-11-20 17:51:20 +0530

    Refactor message and random image functions
    Replaced old functions with new photo randomization logic.

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-        "💗 Dear Neruha, you're special, amazing and this is your mini website surprise! 💗";
-}
+let photos = [
+    "kpop1.jpg",
+    "kpop2.jpg",
+    "kpop.jpg3.jpeg",
+    "neruhapic1.jpg"
+];
 
-function randomKpop() {
-    const pics = [
-        "https://i.imgur.com/VHDy8Xy.jpeg",
-        "https://i.imgur.com/XH6pfaP.jpeg",
-        "https://i.imgur.com/uzFztX4.jpeg",
-        "https://i.imgur.com/tGUXw4l.jpeg"
-    ];
-
-    const img = document.getElementById("random-img");
-    img.src = pics[Math.floor(Math.random() * pics.length)];
+function changePhoto() {
+    let random = Math.floor(Math.random() * photos.length);
+    document.getElementById("mainPhoto").src = photos[random];
 }
